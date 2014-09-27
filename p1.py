9ac7ce9ee4c4c6c60567d45408ea4489847cb06d
leonxlin <leonxlin@gmail.com>
2014-09-27 19:06:37 -0400

    add example funcs and automate gradient descent with different settings

--- p1.py
+++ p1.py
@@ -1,5 +1,7 @@
 import numpy as np
 import math
+import time
+import signal
 
 def gradient_descent(f, guess, step=0.1, thresh=1e-10, g=None):
     # f: scalar function of vector argument to optimize
@@ -63,12 +65,12 @@ def example_func_1_grad(x):
 
 def example_func_2(x):
     """A convex function."""
-    # x: a three-dimensional horizontal vector
-    return (x[0] - 7)**2 + (x[1] - 8.1)**2 + (x[2] - 9.2)**2 + 10.3
+    # x: a two-dimensional horizontal vector
+    return (x[0] - 7)**2 + (x[1] - 8.1)**2
 
 def example_func_2_grad(x):
-    # x: a three-dimensional horizontal vector
-    return np.array([2*(x[0] - 7), 2*(x[1] - 8.1), 2*(x[2] - 9.2)])
+    # x: a two-dimensional horizontal vector
+    return np.array([2*(x[0] - 7), 2*(x[1] - 8.1)])
 
 def example_func_3(x):
     """Ackley's function"""
@@ -82,4 +84,37 @@ def example_func_4(x):
     # x: a two-dimensional horizontal vector
     return - math.cos(x[0]) * math.cos(x[1]) * \
         math.exp(-(x[0]-math.pi)**2 -(x[1]-math.pi)**2)
+
+if __name__ == "__main__":
+    print "here"
+
+    convex_guesses = [[0,0], [3,2], [7,8], [1e6, 1e6]]
+    convex_steps = [1e-3, 1e-2, 1e-1, 1e0, 1e1] 
+    convex_threshes = [1e-10, 1e-5, 1e-1]
+
+    bumpy_guesses = [[0,0], [0.5,0], [0.5,0.5], [1,1], [1,1.5]]
+    bumpy_steps = convex_steps
+    bumpy_threshes = convex_threshes
+
+    def timeout_handler(signum, frame):
+        raise Exception("  TIMEOUT")
+
+    def go(f, guesses, steps, threshes, name):
+        for guess in guesses:
+            for step in steps:
+                for thresh in threshes:
+                    print 'finding minimum of', name
+                    print '  with guess', guess, 'step', step, 'thresh', thresh
+                    # Halt computation after 2 seconds
+                    signal.signal(signal.SIGALRM, timeout_handler)
+                    signal.alarm(2)
+                    try:
+                        x = gradient_descent(f, np.array(guess), step=step, thresh=thresh)
+                        print '  minimum is', f(x), 'attained at x = ', x
+                    except Exception, m:
+                        print m
+                    signal.alarm(0)
+
+    go(example_func_2, convex_guesses, convex_steps, convex_threshes, 'convex example f')
+    go(example_func_3, bumpy_guesses, bumpy_steps, bumpy_threshes, 'bumpy example f')
 
