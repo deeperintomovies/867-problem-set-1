e6cf4e290a3732e5dbb708d87b6bb86ca12a3328
Max H Dunitz <mdunitz@mit.edu>
2014-09-29 17:47:19 -0400

    Fixed bug in sse_derivative, added ridge regression (3-1)

---
+++ problem_2_save.py
@@ -0,0 +1,89 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def getData(name):
+    data = np.loadtxt(name)
+    # Returns column matrices
+    X = data[0:1].T
+    Y = data[1:2].T
+    return X, Y
+
+def max_likelihood(x, y, M):
+    # x: data x coords
+    # y: data y coords
+    # M: fit polynomial of order M
+    # w: regression coeffs---ie, y_ols(x) = w_ols[0] + w_ols[1]*x +...+ w_ols[M]*x^M
+    X = np.array([[i**d for d in range(M+1)] for i in x])#.reshape((len(x), M+1))
+    X_square = (X.T.dot(X))
+    X_inv = np.linalg.inv(X_square)
+    X_final = X_inv.dot(X.T)
+    w_ols = X_final.dot(y)
+    return w_ols
+
+def max_likelihood_ridge(x, y, M, l):
+    # x: data x coords
+    # y: data y coords
+    # M: fit polynomial of order M
+    # l: lambda in ridge regression
+    lambda_I = l*np.identity(M+1)
+    X = np.array([[i**d for d in range(M+1)] for i in x])#.reshape((len(x), M+1))
+    X_square = (X.T.dot(X))
+    X_inv = np.linalg.inv(lambda_I + X_square)
+    X_final = X_inv.dot(X.T)
+    w_ols = X_final.dot(y)
+ 
+
+
+def sse(x, y, w):
+    # x: data x coords
+    # y: data y coords
+    # w: regression coeffs choice---y_hat(x) = w[0]*x**0+...+w[M]*x**M
+    # returns sum of squared errors between ys and y_hats
+    err = 0
+    for i,j in zip(x,y):
+        y_hat = compute_yhat(i, w)
+        err += (j-y_hat)**2
+    return err
+
+def compute_yhat(x_0,w):
+    # x_0: single x coord
+    # w: regression coeffs choice---y_hat(x) = w[0]*x**0+...+w[M]*x**M
+    # returns y_hat
+
+    return sum([w[exp]*(x_0**exp) for exp in range(len(w))])
+
+def get_sse_and_sse_prime(x,y):
+    def f(w):
+        return sse(x,y,w)
+    def fprime(w):
+        return sse_derivative(x,y,w)
+    return (f, fprime)
+
+def sse_derivative(x, y, w):
+    # x: data x coords
+    # y: data y coords
+    # w: regression coeffs choice---y_hat(x) = w[0]*x**0+...+w[M]*x**M
+    # returns gradient vector [d/dw_k sse(x, y, w) for k in range(len(w))]
+
+    residual_vector = np.array([y[i]-compute_yhat(x[i], w) for i in range(len(x))])
+    coeffs = np.array([[-2*x[k]**(i) for i in range(len(x))] for k in range(len(w))])
+    return coeffs.dot(residual_vector)
+
+
+def problem_2(M):
+    x,y = getData('curvefitting.txt')
+    w_ols = max_likelihood(x, y, M)
+    low_limit_x = min(x) - 0.1*abs(max(x)-min(x)) 
+    hi_limit_x = max(x) + 0.1*abs(max(x)-min(x))
+    x_points = np.linspace(low_limit_x, hi_limit_x, 100)
+    y_hat = np.array([compute_yhat(pt, w_ols) for pt in x_points])
+    low_limit_y = min(y) - 0.2*abs(max(y) - min(y))
+    hi_limit_y = max(y) + 0.2*abs(max(y)-min(y))
+    plt.plot(x_points, y_hat, '-b', label='Max Likelihood, M='+str(M))
+    plt.scatter(x, y, label='Data')
+    plt.plot(x_points, np.sin(2*np.pi*x_points), '-', color='0.5', label='Sin(2*pi*x)')
+    plt.xlim(low_limit_x, hi_limit_x)
+    plt.ylim(low_limit_y, hi_limit_y)
+    plt.legend()
+    plt.show()
+    plt.savefig('Problem2_M_'+str(M)+'.png')    
