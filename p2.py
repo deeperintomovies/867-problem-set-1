6daebab2f12d6582c0b7172231ef3ca9a0bfe9da
leonxlin <leonxlin@gmail.com>
2014-09-30 18:17:54 -0400

    bishop_plot clean up

--- p2.py
+++ p2.py
@@ -73,21 +73,21 @@ def problem_2(M):
 
 
 
-def bishop_plot(x,y,w,l,M):
+def bishop_plot(x, y, w, label):
+    # l: a string to be used in the filename
     low_limit_x = min(x) - 0.1*abs(max(x)-min(x)) 
     hi_limit_x = max(x) + 0.1*abs(max(x)-min(x))
     x_points = np.linspace(low_limit_x, hi_limit_x, 100)
     y_hat = np.array([compute_yhat(pt, w) for pt in x_points])
     low_limit_y = min(y) - 0.2*abs(max(y) - min(y))
     hi_limit_y = max(y) + 0.2*abs(max(y)-min(y))
-    plt.plot(x_points, y_hat, '-b', label=l+', M='+str(M))
+    plt.plot(x_points, y_hat, '-b', label=label)
     plt.scatter(x, y, label='Data')
     plt.plot(x_points, np.sin(2*np.pi*x_points), '-', color='0.5', label='Sin(2*pi*x)')
     plt.xlim(low_limit_x, hi_limit_x)
     plt.ylim(low_limit_y, hi_limit_y)
     plt.legend()
     plt.show()
-    plt.savefig('Problem2_'+l+'M_'+str(M)+'.png')   
 
 def problem_2_1(max_M=15):
     sse_M = []
@@ -97,15 +97,16 @@ def problem_2_1(max_M=15):
         x = x.ravel()
         y = y.ravel()
         w_ols = max_likelihood(x,y,i)
-        bishop_plot(x,y,w_ols,'MaxLikelihood'+str(i),i)
+        bishop_plot(x,y,w_ols,'maximum likelihood, M = %s' % i)
+        plt.savefig('problem2_ml_M_%s.png' % i)
         sse_M.append(sse(x,y,w_ols))
         l_2_norm.append(sum([ii**2 for ii in w_ols])**0.5)
     plt.close()
     plt.plot(np.array(range(max_M)), np.array(sse_M), label='Sum of Squared Errors')
-    plt.savefig('Problem2_OLS_SSEvsM.png')
+    plt.savefig('problem2_OLS_SSEvsM.png')
     plt.close()
     plt.plot(np.array(range(max_M)), np.array(l_2_norm), label='L2 Norm of Weight Vector')
-    plt.savefig('Problem2_OLS_L2_NormvsM.png')
+    plt.savefig('problem2_OLS_L2_NormvsM.png')
     plt.close()
     x,y = getData('curvefitting.txt')
     x = x.ravel()
@@ -116,7 +117,8 @@ def problem_2_1(max_M=15):
         y = np.delete(y,ii)
     for M in [0,1,3,9]:
         w_ols = max_likelihood(x.ravel(),y.ravel(),M)
-        bishop_plot(x,y,w_ols,'MaxLikelihoodDataMissing', M)
+        bishop_plot(x,y,w_ols,'maximum likelihood w/missing, M = %s' % M)
+        plt.savefig('problem2_mlmissing_M_%s.png' % M)
 
 if __name__ == '__main__':
     problem_2_1()
