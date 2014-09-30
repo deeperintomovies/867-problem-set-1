08094ed1c79139f3d11d86e0d9188853906015ea
leonxlin <leonxlin@gmail.com>
2014-09-30 18:25:05 -0400

    ridge regeression on bishop data

--- p3.py
+++ p3.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from util import getData
+from p2 import bishop_plot, compute_yhat
 
 def max_likelihood_ridge(x, y, M, l):
     # x: data x coords
@@ -16,3 +17,16 @@ def max_likelihood_ridge(x, y, M, l):
     w_ridge = X_final.dot(y)
     return w_ridge
 
+
+def problem_3_1():
+    x, y = getData('curvefitting.txt')
+    x = x.ravel()
+    y = y.ravel()
+    for M in [3, 4, 5, 7, 9]:
+        for lam in [0, 0.001, 0.01, 0.1]:
+            w_ridge = max_likelihood_ridge(x, y, M, lam)
+            bishop_plot(x, y, w_ridge, 'ridge regression, M=%s, lam=%s' % (M, lam))
+            plt.savefig('problem3_1_M_%s_lam_%s.png' % (M, lam))
+
+if __name__ == '__main__':
+    problem_3_1()
