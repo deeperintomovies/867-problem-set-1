a621516fd04800d8e694835e8bd14216265fead5
leonxlin <leonxlin@gmail.com>
2014-09-30 23:30:47 -0400

    misc

--- p3.py
+++ p3.py
@@ -32,7 +32,7 @@ def problem_3_3():
 
 def problem_3_1():
     x, y = getData('curvefitting.txt')
-    for M in [3, 4, 5, 6, 7, 8, 9]:
+    for M in [1, 2, 3]:
         for lam in [0, 0.0003, 0.001, 0.003]:
             w_ridge = polynomial_ridge(x, y, M, lam)
             bishop_plot(x, y, w_ridge, 'ridge regression, M=%s, lam=%s' % (M, lam))
@@ -40,24 +40,56 @@ def problem_3_1():
             print 'just tried M = %s, lam=%s' % (M, lam)
             print '    sse was %s' % sse(x, y, w_ridge)
 
-def problem_3_2(A_or_B, plot=False):
+def problem_3_2_case(train_x, train_y, valid_x, valid_y, M, lam, A_or_B, plot=False):
+    w_ridge = polynomial_ridge(train_x, train_y, M, lam)
+    xs_to_plot = get_xs_to_plot(train_x.tolist() + valid_x.tolist())
+    y_hat = np.array([compute_yhat(x, w_ridge) for x in xs_to_plot])
+    error = sse(valid_x, valid_y, w_ridge)
+    if plot:
+        plt.close()
+        plt.scatter(train_x, train_y, color='blue', label='train ' + A_or_B)
+        plt.scatter(valid_x, valid_y, color='red', label='validate')
+        plt.plot(xs_to_plot, y_hat, color='black', 
+            label='ridge regression, $M=%s$, $\lambda=%s$' % (M, lam))
+        plt.xlabel('$x$')
+        plt.ylabel('$y$')
+        plt.legend(loc=4)
+        plt.savefig('problem3_2_train%s_M_%s_lam_%s.png' % (A_or_B, M, lam))
+        plt.show()
+    print 'just tried train=%s, M=%s, lam=%2.2f: sse=%3.4f' % \
+        (A_or_B, M, lam, error)
+    return error
+
+def problem_3_2(A_or_B, plot_individual=False):
     train_x, train_y = getData('regress%s_train.txt' % A_or_B)
     valid_x, valid_y = getData('regress_validate.txt')
+    lams = np.linspace(0, 10, 101)
+    colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k', '#808080', '#DDDD00', '#800000']
+    best_lams = []
+    best_sses = []
     for M in range(10):
-        for lam in [0, 0.1, 0.5, 1, 5, 10]:
-            w_ridge = polynomial_ridge(train_x, train_y, M, lam)
-            xs_to_plot = get_xs_to_plot(train_x.tolist() + valid_x.tolist())
-            y_hat = np.array([compute_yhat(x, w_ridge) for x in xs_to_plot])
-            if plot:
-                plt.close()
-                plt.scatter(train_x, train_y, color='blue', label='train ' + A_or_B)
-                plt.scatter(valid_x, valid_y, color='red', label='validate')
-                plt.plot(xs_to_plot, y_hat, color='black', 
-                    label='ridge on train, M=%s, lam=%s' % (M, lam))
-                plt.legend()
-                plt.savefig('problem3_2_train%s_M_%s_lam_%s.png' % (A_or_B, M, lam))
-            print 'just tried train=%s, M=%s, lam=%2.2f: sse=%3.4f' % \
-                (A_or_B, M, lam, sse(valid_x, valid_y, w_ridge))
+        errors = []
+        for lam in lams:
+            error = problem_3_2_case(train_x, train_y, valid_x, valid_y,
+                M, lam, A_or_B, plot_individual)
+            errors.append(error)
+        if not plot_individual:
+            plt.plot(lams, errors, color=colors[M], 
+                label='M=%s' % M)
+        best_lams.append(np.argmin(errors)/10.)
+        best_sses.append(min(errors))
+    print best_lams
+    print best_sses
+    if not plot_individual:
+        plt.ylim(0, 30 if A_or_B == 'A' else 100)
+        plt.title('Ridge regression error by $M$ and $\lambda$, training set %s' % A_or_B)
+        plt.xlabel('$\lambda$')
+        plt.ylabel('sum of squared error (against validation data)')
+        plt.legend()
+        plt.savefig('problem3_2_train%s_meta.png' % A_or_B)
+        plt.show()
+        plt.close()
+        
 
 if __name__ == '__main__':
     #problem_3_2('A')
